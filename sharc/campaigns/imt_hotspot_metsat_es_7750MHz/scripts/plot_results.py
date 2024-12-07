b50ad5017601a5d0df8dba83373357689fc7c256
artistrea <apiravesi@gmail.com>
2024-12-07 18:53:10 -0300

    wip(campaigns): metsat es campaign plot_results

--- sharc/campaigns/imt_hotspot_metsat_es_7750MHz/scripts/plot_results.py
+++ sharc/campaigns/imt_hotspot_metsat_es_7750MHz/scripts/plot_results.py
@@ -22,16 +22,37 @@ post_processor\
         dir_name_contains="non_gso_sat_E_and_G_7780_5km",
         legend="ES for Sat. E and G (7780 MHz) (5km)"
     ).add_plot_legend_pattern(
-        dir_name_contains="sat_jpss_1_5000m",
-        legend="ES for Sat. JPSS-1 (5km)"
+        dir_name_contains="non_gso_sat_E_and_G_7780_0km",
+        legend="ES for Sat. E and G (7780 MHz) (0km)"
     ).add_plot_legend_pattern(
-        dir_name_contains="non_gso_sat_E_and_G_7780_5000m",
-        legend="ES for Sat. E and G (7780 MHz) (5km)"
+        dir_name_contains="non_gso_sat_E_and_G_7780_1km",
+        legend="ES for Sat. E and G (7780 MHz) (1km)"
+    ).add_plot_legend_pattern(
+        dir_name_contains="non_gso_sat_E_and_G_7780_2km",
+        legend="ES for Sat. E and G (7780 MHz) (2km)"
+    ).add_plot_legend_pattern(
+        dir_name_contains="non_gso_sat_E_and_G_7780_3km",
+        legend="ES for Sat. E and G (7780 MHz) (3km)"
+    ).add_plot_legend_pattern(
+        dir_name_contains="non_gso_sat_E_and_G_7780_4km",
+        legend="ES for Sat. E and G (7780 MHz) (4km)"
+    ).add_plot_legend_pattern(
+        dir_name_contains="non_gso_sat_C_and_S_4km",
+        legend="ES for Sat. C and S (4km)"
+    ).add_plot_legend_pattern(
+        dir_name_contains="non_gso_sat_C_and_S_3km",
+        legend="ES for Sat. C and S (3km)"
+    ).add_plot_legend_pattern(
+        dir_name_contains="non_gso_sat_C_and_S_2km",
+        legend="ES for Sat. C and S (2km)"
+    ).add_plot_legend_pattern(
+        dir_name_contains="non_gso_sat_C_and_S_1km",
+        legend="ES for Sat. C and S (1km)"
+    ).add_plot_legend_pattern(
+        dir_name_contains="non_gso_sat_C_and_S_0km",
+        legend="ES for Sat. C and S (0km)"
     ).add_plot_legend_pattern(
         dir_name_contains="non_gso_sat_C_and_S_5km",
-        legend="ES for Sat. C and S (5km)"
-    ).add_plot_legend_pattern(
-        dir_name_contains="non_gso_sat_C_and_S_5000m",
         legend="ES for Sat. C and S (5km)"
     )
 
@@ -45,9 +66,9 @@ attributes_to_plot = [
 
 def filter_fn(x):
     return True
-    return ("E_and_G" in x
-        or "C_and_S" in x
-    ) and ("5000m" in x or "5km" in x)
+    # return ("E_and_G" in x
+    #     or "C_and_S" in x
+    # ) and ("5000m" in x or "5km" in x)
 
 dl_results = Results.load_many_from_dir(
     dl_dir, only_latest=True,
@@ -67,114 +88,117 @@ all_results = [
     # *ul_results
 ]
 
-post_processor.add_results(all_results)
 
-plots = post_processor.generate_cdf_plots_from_results(
-    all_results
+post_processor.add_plots(
+    post_processor.generate_ccdf_plots_from_results(
+        all_results
+    )
+)
+post_processor.add_plots(
+    post_processor.generate_cdf_plots_from_results(
+        all_results
+    )
 )
 
-post_processor.add_plots(plots)
-
-# Show a single plot:
-system_dl_interf_power_plot = post_processor\
-    .get_plot_by_results_attribute_name("system_dl_interf_power_per_mhz")
-
-system_ul_interf_power_plot = post_processor\
-    .get_plot_by_results_attribute_name("system_ul_interf_power_per_mhz")
-
+# Add a protection criteria line:
+# dB to dBm (+ 30)
+# the following conversion makes the criteria more strict, so there may not be a problem
+plots_to_add_vline = [
+    "system_ul_interf_power_per_mhz",
+    "system_dl_interf_power_per_mhz"
+]
 
 # Add a protection criteria line:
 # dB to dBm (+ 30)
 # the following conversion makes the criteria more strict, so there may not be a problem
 # / 10MHz to / 1MHz  (-10)
-interf_protection_criteria = -148 + 30 - 10
-if system_dl_interf_power_plot:
-    system_dl_interf_power_plot.add_vline(
-        interf_protection_criteria, line_dash="dash",
-        name="20% criteria"
-    )
+interf_protection_criteria0 = -127 + 30 - 10
+interf_protection_criteria1 = -148 + 30 - 10
 
-if system_ul_interf_power_plot:
-    system_ul_interf_power_plot.add_vline(
-        interf_protection_criteria, line_dash="dash",
-        name="20% criteria"
-    )
+for prop_name in plots_to_add_vline:
+    for plot_type in ["cdf", "ccdf"]:
+        plt = post_processor\
+            .get_plot_by_results_attribute_name(prop_name, plot_type=plot_type)
+        if plt:
+            plt.add_vline(
+                interf_protection_criteria0, line_dash="dash",
+                name="0.0016% criteria"
+            )
+            plt.add_vline(
+                interf_protection_criteria1, line_dash="dash",
+                name="20% criteria"
+            )
 
-interf_protection_criteria = -127 + 30 - 10
+# system_dl_interf_power_plot = post_processor\
+#     .get_plot_by_results_attribute_name("system_dl_interf_power_per_mhz")
 
-if system_dl_interf_power_plot:
-    system_dl_interf_power_plot.add_vline(
-        interf_protection_criteria, line_dash="dash",
-        name="0.0016% criteria"
-    )
+# system_ul_interf_power_plot = post_processor\
+#     .get_plot_by_results_attribute_name("system_ul_interf_power_per_mhz")
 
-if system_ul_interf_power_plot:
-    system_ul_interf_power_plot.add_vline(
-        interf_protection_criteria, line_dash="dash",
-        name="0.0016% criteria"
-    )
+# i = 0
+# antenna_bs = None
+# antenna_ue = None
 
-i = 0
-antenna_bs = None
-antenna_ue = None
+# antenna_radiation_plot  = None
+# for result in all_results:
+#     if "_5000m_" not in result.output_directory and "_5km_" not in result.output_directory:
+#         continue
 
-antenna_radiation_plot  = None
+#     params_file = glob.glob(result.output_directory + "/*.yaml")[0]
+#     params = Parameters()
+#     params.set_file_name(params_file)
+#     params.read_params()
 
-for result in all_results:
-    if "_5000m_" not in result.output_directory and "_5km_" not in result.output_directory:
-        continue
+#     # TODO: use antenna factory here if it ever exists
+#     legend = post_processor.get_results_possible_legends(result)[0]
+#     if params.single_earth_station.antenna.pattern == "ITU-R S.465":
+#         antenna = AntennaS465(params.single_earth_station.antenna.itu_r_s_465)
+#     if params.single_earth_station.antenna.pattern == "ITU-R S.580":
+#         antenna = AntennaS580(params.single_earth_station.antenna.itu_r_s_580)
+#     antenna_radiation_plot = PostProcessor.generate_antenna_radiation_pattern_plot(
+#         plot_title="Antenna Radiation Patterns",
+#         antenna=antenna, legend=legend["legend"][:-6], plot=antenna_radiation_plot,
+#     )
 
-    params_file = glob.glob(result.output_directory + "/*.yaml")[0]
-    params = Parameters()
-    params.set_file_name(params_file)
-    params.read_params()
+#     if i == 0:
+#         antenna_bs = AntennaBeamformingImt(
+#             params.imt.bs.antenna.get_antenna_parameters(),
+#             0,
+#             0,
+#             # -params.imt.bs.antenna.downtilt
+#         )
+#         antenna_ue = AntennaBeamformingImt(
+#             params.imt.ue.antenna.get_antenna_parameters(),
+#             0,
+#             0
+#         )
 
-    # TODO: use antenna factory here if it ever exists
-    legend = post_processor.get_results_possible_legends(result)[0]
-    if params.single_earth_station.antenna.pattern == "ITU-R S.465":
-        antenna = AntennaS465(params.single_earth_station.antenna.itu_r_s_465)
-    if params.single_earth_station.antenna.pattern == "ITU-R S.580":
-        antenna = AntennaS580(params.single_earth_station.antenna.itu_r_s_580)
-    antenna_radiation_plot = PostProcessor.generate_antenna_radiation_pattern_plot(
-        plot_title="Antenna Radiation Patterns",
-        antenna=antenna, legend=legend["legend"][:-6], plot=antenna_radiation_plot,
-    )
+#     i += 1
 
-    if i == 0:
-        antenna_bs = AntennaBeamformingImt(
-            params.imt.bs.antenna.get_antenna_parameters(),
-            0,
-            0,
-            # -params.imt.bs.antenna.downtilt
-        )
-        antenna_ue = AntennaBeamformingImt(
-            params.imt.ue.antenna.get_antenna_parameters(),
-            0,
-            0
-        )
+# antenna_radiation_plot.show()
 
-    i += 1
-
-antenna_radiation_plot.show()
-
-for pl_name in attributes_to_plot:
-    plot = post_processor\
-        .get_plot_by_results_attribute_name(pl_name)
-    if plot:
-        plot.show()
+PostProcessor.save_plots(
+    os.path.join(campaign_base_dir, "output", "figs"),
+    post_processor.plots,
+)
+# for pl_name in attributes_to_plot:
+#     plot = post_processor\
+#         .get_plot_by_results_attribute_name(pl_name)
+#     if plot:
+#         plot.show()
 
 
-plot_antenna_imt = PlotAntennaPattern("")
+# plot_antenna_imt = PlotAntennaPattern("")
 
-# Plot BS TX radiation patterns
-f = plot_antenna_imt.plot_element_pattern(antenna_bs, "BS", "ELEMENT")
-# f.savefig(figs_dir + "BS_element.pdf", bbox_inches='tight')
-f = plot_antenna_imt.plot_element_pattern(antenna_bs, "TX", "ARRAY")
-# f.savefig(figs_dir + "BS_array.pdf", bbox_inches='tight')
+# # Plot BS TX radiation patterns
+# f = plot_antenna_imt.plot_element_pattern(antenna_bs, "BS", "ELEMENT")
+# # f.savefig(figs_dir + "BS_element.pdf", bbox_inches='tight')
+# f = plot_antenna_imt.plot_element_pattern(antenna_bs, "TX", "ARRAY")
+# # f.savefig(figs_dir + "BS_array.pdf", bbox_inches='tight')
 
-# Plot UE TX radiation patterns
-plot_antenna_imt.plot_element_pattern(antenna_ue, "UE", "ELEMENT")
-plot_antenna_imt.plot_element_pattern(antenna_ue, "UE", "ARRAY")
+# # Plot UE TX radiation patterns
+# plot_antenna_imt.plot_element_pattern(antenna_ue, "UE", "ELEMENT")
+# plot_antenna_imt.plot_element_pattern(antenna_ue, "UE", "ARRAY")
 
 full_results = ""
 
@@ -194,7 +218,6 @@ with open(dl_dir + "/stats.txt", "w") as fl:
 
 
 # # Aggregate results.
-# # TODO: Need to check if building entry loss needs update before simulating UL
 
 # aggregated_plot = None
 # if system_ul_interf_power_plot and system_dl_interf_power_plot:
